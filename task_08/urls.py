806c22044f116727f8683fcf6b383dbfd0265927
Alyaa AlOstad <alyaaalostad@hotmail.com>
2019-08-26 14:41:53 +0300

    Samira and Alyaa

--- task_08/urls.py
+++ task_08/urls.py
@@ -30,5 +30,9 @@ urlpatterns = [
     path('restaurants/delete/<int:restaurant_id>/',views.restaurant_delete ,name='restaurant-delete'),
 ]
 
+
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
